a1dd0a70aefb09764719c29c603e1b076c2b697e
Bruce Yan <kuny@umich.edu>
2019-11-26 11:11:02 -0500

    first commit

---
+++ HW123/HW123/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  HW123
+//
+//  Created by Bruce Yan on 11/22/19.
+//  Copyright © 2019 Bruce Yan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+class superman{
+    var age = 10000
+    
+    init(){
+        print("superman is the best")
+    }
+}
+
+for n in 1...100{
+    print(n)
+}
